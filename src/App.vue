4f7e5884e064d6058effdaeb8aff68170b949337
tyttseng <tyttseng@gmail.com>
2021-04-18 15:11:04 +0800

    Revert "Add Products view and components"
    This reverts commit 0274784915814fa1575bb593b4476105803b97ac.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div id="ecommerce"></div>
+  <div id="app">
+    <Navbar />
+    <main role="main" class="mt-5 bg-white">
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import Navbar from "./components/Navbar";
+import { mapActions } from "vuex";
+
 export default {
   name: "App",
+  components: {
+    Navbar,
+  },
+  methods: {
+    ...mapActions(["fetchProducts", "fetchCartProducts"]),
+  },
+  mounted() {
+    this.fetchProducts();
+    this.fetchCartProducts();
+  },
 };
 </script>
