eff9895dc2a7e87b987812648c8056ba66180395
tyttseng <tyttseng@gmail.com>
2021-06-10 16:37:13 +0800

    optimize code

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,12 @@
+// 程式碼壓縮
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/e-commerce-v2/'
     : '/',
-  productionSourceMap: false,
-  // process.env.NODE_ENV !== 'production', // production 時為 false
+  productionSourceMap: process.env.NODE_ENV !== 'production',
+
   chainWebpack: config => {
     // ============壓縮圖片 start============
     config.module
@@ -13,5 +16,24 @@ module.exports = {
       .options({ bypassOnDebug: true })
       .end()
     // ============壓縮圖片 end============
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      // 程式碼壓縮
+      config.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            //生產環境自動刪除console
+            compress: {
+              warnings: false, // 若打包錯誤，則註釋這行
+              drop_debugger: true,
+              drop_console: true,
+              pure_funcs: ['console.log']
+            }
+          },
+          sourceMap: false,
+          parallel: true
+        })
+    }
   }
 }
