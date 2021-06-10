94a482a1d9bbc4cb3b370da827313ccecd70a2dc
tyttseng <tyttseng@gmail.com>
2021-06-10 16:17:56 +0800

    optimize image

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/e-commerce-v2/'
-    : '/'
+    : '/',
   productionSourceMap: false,
   // process.env.NODE_ENV !== 'production', // production 時為 false
   chainWebpack: config => {
