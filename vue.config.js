7f3c32d9cf4d66494dd20d80b68076391fbb556b
tyttseng <tyttseng@gmail.com>
2021-07-13 21:10:55 +0800

    add webpack optimization config

--- vue.config.js
+++ vue.config.js
@@ -41,6 +41,40 @@ module.exports = {
           parallel: true
         })
       )
+      // 公共程式碼抽離
+      config.optimization = {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              chunks: 'all',
+              test: /node_modules/,
+              name: 'vendor',
+              minChunks: 1,
+              maxInitialRequests: 5,
+              minSize: 0,
+              priority: 100
+            },
+            common: {
+              chunks: 'all',
+              test: /[/]src[/]js[/]/,
+              name: 'common',
+              minChunks: 2,
+              maxInitialRequests: 5,
+              minSize: 0,
+              priority: 60
+            },
+            styles: {
+              name: 'styles',
+              test: /.(sa|sc|c)ss$/,
+              chunks: 'all',
+              enforce: true
+            },
+            runtimeChunk: {
+              name: 'manifest'
+            }
+          }
+        }
+      }
     }
   }
 }
